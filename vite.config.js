b08f9343781511e30d0520c7e885c0445ae55d41
Doan vo van trong <trongdn2405@gmail.com>
2024-05-12 10:53:15 +0700

    Fix view commit --done

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      "@": path.resolve(__dirname, "./src"),
     }
   }
 })
